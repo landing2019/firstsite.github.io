f7195583a485a13933bd82d75e6e5db29cc77477
landing2019 <52202683+landing2019@users.noreply.github.com>
2019-07-01 14:57:37 +0300

    Update jquery.main.js

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -9,3 +9,17 @@ $('[data-fancybox="gallery"]').fancybox({
 	// Options will go here
 });
 
+$(function(){
+
+  'use strict';
+
+  $('a[href^="#"]').click(function(){
+    let target = $(this).attr('href');
+    $('html, body').animate(
+      {
+        scrollTop:$(target).offset().top
+      },
+      800
+    );
+  });
+});
