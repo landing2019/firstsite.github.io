88abac00c2ffde0cb052ef384e94e2fd1f653ebd
landing2019 <52202683+landing2019@users.noreply.github.com>
2019-07-01 14:58:09 +0300

    Update jquery.main.js

--- one/js/jquery.main.js
+++ one/js/jquery.main.js
@@ -4,3 +4,18 @@ $('.menu-btn').on('click', function(e) {
   $('.menu-nav').toggleClass('menu-nav_active');
   $('.item').toggleClass('item_active');
 });
+
+$(function(){
+
+  'use strict';
+
+  $('a[href^="#"]').click(function(){
+    let target = $(this).attr('href');
+    $('html, body').animate(
+      {
+        scrollTop:$(target).offset().top
+      },
+      800
+    );
+  });
+});
