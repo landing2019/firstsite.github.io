bf90128c0b48e2b7de69441702c875ff44d4b2e1
landing2019 <khadsl17140@gmail.com>
2019-07-01 16:35:18 +0300

    upgrade

--- twosite/js/jquery.main.js
+++ twosite/js/jquery.main.js
@@ -4,3 +4,27 @@ $('.menu-btn').on('click', function(e) {
   $('.menu-nav').toggleClass('menu-nav_active');
   $('.item').toggleClass('item_active');
 });
+
+$(function(){
+
+  'use strict';
+
+  $('a[href^="#"]').click(function(){
+    let target = $(this).attr('href');
+    $('html, body').animate(
+      {
+        scrollTop:$(target).offset().top
+      },
+      800
+    );
+  });
+
+  $('#slide').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  });
+
+});
+
+
